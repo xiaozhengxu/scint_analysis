626fd7371c6a2c6cd75c7436a45616eab7656b40
Judy <judy.xu1996@hotmail.com>
2016-05-13 13:27:58 -0400

    modifying small changes

--- EVN_crab/GPbaseband.py
+++ EVN_crab/GPbaseband.py
@@ -6,30 +6,6 @@ import matplotlib.pyplot as plt
 from astropy.time import Time
 
 '''
-i = 13
-j = 15
-text_name = 'all20.txt'
-with open(text_name, 'r') as f:
-	text = f.read()
-	text_lines = text.split('\n')
-strings1 = text_lines[i-1].split()
-strings2 = text_lines[j-1].split()
-if text_name[0] == 'a': # get information from a all__.txt file
-	scan_no1 = strings1[1]
-	scan_no2 = strings2[1]
-	t1 = strings1[2]
-	t2 = strings2[2]
-elif text_name[0] == 's': # get information from a scan__.txt file
-	scan_no1 = text_name[4:6]
-	scan_no2 = text_name[4:6]
-	t1 = strings1[0]
-	t2 = strings2[0]
-print scan_no1,t1,scan_no2,t2
-
-fn1 = '/cita/h/home-2/xzxu/trails/data/ek036a_ef_no00{}.m5a'.format(scan_no1)
-fn2 = '/cita/h/home-2/xzxu/trails/data/ek036a_ef_no00{}.m5a'.format(scan_no2)
-t_gp1 = Time(t1)
-t_gp2 = Time(t2)
 '''
 
 size = 2 ** 22
@@ -81,19 +57,40 @@ class DispersionMeasure(u.Quantity):
 
 dm = DispersionMeasure(dm)
 
+def get_SN(outputsumfreq):
+	noise_std = np.std(outputsumfreq[:1000]) 
+	noise_mean = np.mean(outputsumfreq[:1000])
+	sigs_noise = (outputsumfreq-noise_mean)/noise_std
+	return sigs_noise
 
 def get_correlation_coefficients(gp1,gp2):
 	coefficients= []
 	for i in range(8):
 		X = gp1.freq_specs[i]
 		Y = gp2.freq_specs[i]
-		#coefficient = np.dot((X-np.mean(X)),(Y-np.mean(Y)))/(np.std(X)*np.std(Y))
 		coefficient = np.corrcoef(X, Y)[0,1] # np.corrcoef returns a matrix [1, coeff;coeff,1]
 		coefficients.append(coefficient) 
 	return np.mean(coefficients),coefficients
 
+def get_correlation_coefficient(gp1,gp2):
+	return np.corrcoef(gp1.freq_spec,gp2.freq_spec)[0,1]
+
+def overlap_freq_spec(gp1,gp2):
+	f, axarr = plt.subplots(8, 1)
+	frequency_interval = 2056/8
+	for i in range(8):
+		freqs = np.linspace((i)*16,(i+1)*16,frequency_interval)+1610.49 #actual frequencies 
+		axarr[i].plot(freqs,gp1.freq_spec[i*frequency_interval:(i+1)*frequency_interval])
+		axarr[i].plot(freqs,gp2.freq_spec[i*frequency_interval:(i+1)*frequency_interval])
+		if i%2==0:
+			axarr[i].set_ylabel('Intensity of peak with that frequency') 
+	axarr[i].set_xlabel('frequency (MHz)')
+	axarr[0].set_title('Frequency spectrum of giant pulses at around '+gp1.t_gp.value+' and '+gp2.t_gp.value)
+	plt.show()
+
 class GP_data(object):
 	def __init__(self,fn,t_gp):
+		self.t_gp = t_gp
 		fh = mark5b.open(fn, mode='rs', nchan=16,
 					    sample_rate=sample_rate, thread_ids=thread_ids, ref_mjd=57000)
 		offset_gp = ((t_gp - fh.tell(unit='time')).to(u.s).value *
@@ -102,7 +99,7 @@ class GP_data(object):
 		self.d_dispersed = fh.read(size)
 		
 		self.process_data()
-		self.get_output()
+		self.process_output()
 		
 	def process_data(self):
 		ft = np.fft.rfft(self.d_dispersed, axis=0)
@@ -115,83 +112,122 @@ class GP_data(object):
 		self.d_dedispersed = np.fft.irfft(ft, axis=0)
 
 		# Channelize the data
-		self.dchan = np.fft.rfft(self.d_dedispersed.reshape(-1, 2*nchan, 16), axis=1)
+		self.dchan = np.fft.rfft(self.d_dedispersed.reshape(-1, 2*nchan, 16), axis=1) #axis 1 means across the 257 values in each time bin? 
 		# Horribly inelegant, but works for now. 
 		# Channels are not in order, and polarizations are separate
 		self.dR = np.concatenate((self.dchan[:,::-1,8], self.dchan[...,0], self.dchan[:,::-1,10], self.dchan[...,2], self.dchan[:,::-1,12], self.dchan[...,4], self.dchan[:,::-1,14], self.dchan[...,6]), axis=1)
 		self.dL = np.concatenate((self.dchan[:,::-1,9], self.dchan[...,1], self.dchan[:,::-1,11], self.dchan[...,3], self.dchan[:,::-1,13], self.dchan[...,5], self.dchan[:,::-1,15], self.dchan[...,7]), axis=1)
-		#plt.ion()
-	def get_output(self):
 		self.output = (abs(self.dR)**2 + abs(self.dL)**2).T
+		
+		self.dchan_1 = np.fft.rfft(self.d_dispersed.reshape(-1, 2*nchan, 16), axis=1) # the dispersed spectrum
+		self.dR1 = np.concatenate((self.dchan_1[:,::-1,8], self.dchan_1[...,0], self.dchan_1[:,::-1,10], self.dchan_1[...,2], self.dchan_1[:,::-1,12], self.dchan_1[...,4], self.dchan_1[:,::-1,14], self.dchan_1[...,6]), axis=1)
+		self.dL1 = np.concatenate((self.dchan_1[:,::-1,9], self.dchan_1[...,1], self.dchan_1[:,::-1,11], self.dchan_1[...,3], self.dchan_1[:,::-1,13], self.dchan_1[...,5], self.dchan_1[:,::-1,15], self.dchan_1[...,7]), axis=1)
+		self.output1 = (abs(self.dR1)**2 + abs(self.dL1)**2).T
+		
+	def process_output(self):
 		self.outputsumfreq = self.output.sum(0) #a (8192,) matrix of the sum over all frequencies at given times
+		self.outputsumfreq1 = self.output1.sum(0)
 		peak_time = np.argmax(self.outputsumfreq) 
-		#a matrix containing only the pulse at 3989 time bin and 3990
+		#a matrix containing only the pulse at pulse time bin and 3990
 		self.output_pulse = self.output[:,peak_time:peak_time+1]    
 		self.freq_spec = self.output_pulse.sum(1) 
 		self.freq_specs = []
 		for i in range(8):
 			self.freq_specs.append(self.freq_spec[18+i*220+i*36:18+(i+1)*220+i*36]) #the values 220 and 36 cuts the beginning and end of each frequency band
 		#computes signal to noise after summing all the frequencies
-		self.noise_std = np.std(self.outputsumfreq[:1000]) 
-		self.noise_mean = np.mean(self.outputsumfreq[:1000])
-		self.sigs_noise = (self.outputsumfreq-self.noise_mean)/self.noise_std
+		self.sigs_noise = get_SN(self.outputsumfreq)
+		self.sigs_noise1 = get_SN(self.outputsumfreq1)
 		self.S_N = max(self.sigs_noise)
-	
+
+		
 	def plot_figs(self):
+		
 		#plots the frequency spectrum: 
 		plt.close('all')
 		f, axarr = plt.subplots(8, 1)
 		frequency_interval = 2056/8
 		for i in range(8):
 			freqs = np.linspace((i)*16,(i+1)*16,frequency_interval)+1610.49 #actual frequencies 
-			freqs = np.linspace((i)*frequency_interval,(i+1)*frequency_interval,frequency_interval)
+			#freqs = np.linspace((i)*frequency_interval,(i+1)*frequency_interval,frequency_interval)
 			axarr[i].plot(freqs,self.freq_spec[i*frequency_interval:(i+1)*frequency_interval])
 			if i%2==0:
 				axarr[i].set_ylabel('Intensity of peak with that frequency') 
 		axarr[i].set_xlabel('frequency (MHz)')
+		axarr[0].set_title('Frequency spectrum of giant pulse at around '+self.t_gp.value)
 		
 		#plots the dynamic spectrum de-dispersed: 
 		plt.figure()
-		#plt.imshow(self.output, aspect='auto',extent=(-8*8192/1000,8*8192/1000,1610.49+16*8,1610.49))
-		plt.imshow(self.output, aspect='auto',extent=(0,8192,1610.49+16*8,1610.49))
+		plt.imshow(self.output, aspect='auto',extent=(-8*8192/1000,8*8192/1000,1610.49+16*8,1610.49))
 		plt.xlabel('time (ms)')
 		plt.ylabel('frequency (MHz)')
-		plt.title('dynamic spectrum of de-dispersed giant pulse')
+		plt.title('dynamic spectrum of de-dispersed giant pulse around '+self.t_gp.value)
 		plt.colorbar()
 
-		#plots the dynamic specturm dispersed as separate subplots for each chanel:
-		self.dchan_1 = np.fft.rfft(self.d_dispersed.reshape(-1, 2*nchan, 16), axis=1) # the dispersed spectrum
-		self.dR1 = np.concatenate((self.dchan_1[:,::-1,8], self.dchan_1[...,0], self.dchan_1[:,::-1,10], self.dchan_1[...,2], self.dchan_1[:,::-1,12], self.dchan_1[...,4], self.dchan_1[:,::-1,14], self.dchan_1[...,6]), axis=1)
-		self.dL1 = np.concatenate((self.dchan_1[:,::-1,9], self.dchan_1[...,1], self.dchan_1[:,::-1,11], self.dchan_1[...,3], self.dchan_1[:,::-1,13], self.dchan_1[...,5], self.dchan_1[:,::-1,15], self.dchan_1[...,7]), axis=1)
-		self.output1 = (abs(self.dR1)**2 + abs(self.dL1)**2).T
+		#plots the dynamic specturm dispersed as a single stiched plot:
 		plt.figure()
-		#plt.imshow(self.output, aspect='auto',extent=(-8*8192/1000,8*8192/1000,1610.49+16*8,1610.49)) #actual times and frequencies
-		plt.imshow(self.output1, aspect='auto',extent=(0,8192,1610.49+16*8,1610.49))# matrix value times
+		plt.imshow(self.output1, aspect='auto',extent=(-8*8192/1000,8*8192/1000,1610.49+16*8,1610.49))#actual times and frequencies
+		plt.title('dynamic spectrum of dispersed giant pulse around '+self.t_gp.value+'separated') 
 		plt.xlabel('time (ms)')
 		plt.ylabel('frequency (MHz)')
-		plt.title('dynamic spectrum of dispersed giant pulse')
+		plt.title('dynamic spectrum of dispersed giant pulse around '+self.t_gp.value)
 		plt.colorbar()
 		
+		#plots the dynamic specturm dispersed as separate subplots for each chanel:
 		f, axarr = plt.subplots(8, 2)
 		for i in range(16):
 			axarr[i%8,i/8].imshow((abs(self.dchan_1[...,i])**2).T,aspect = 'auto')
 			axarr[i%8,i/8].set_title('chanel {}'.format(i))
 		axarr[i%8,i/8].set_title('dispersed pulse in its 16 channels')
 		
-		#plots the signal vs noise
+		#plots the signal vs noise of de-dispersed
 		plt.figure() 
 		plt.plot(self.sigs_noise)
 		plt.xlabel('time ($16 \mu s$)')
 		plt.ylabel('S/N')
+		plt.title('Signal to noise of de-dispersed pulse around '+self.t_gp.value)
 		
+		#plots the signal vs noise of dispersed - looks like noise, so won't plot
+		#'''
+		plt.figure() 
+		plt.plot(self.sigs_noise1)
+		plt.xlabel('time ($16 \mu s$)')
+		plt.ylabel('S/N')
+		plt.title('Signal to noise of dispersed pulse around '+self.t_gp.value)
 		plt.show()
-		
-    
+   		
 if __name__ == "__main__":
+	i = 1
+	j = 2
+	text_name = 'all30.txt'
+	with open(text_name, 'r') as f:
+		text = f.read()
+		text_lines = text.split('\n')
+	strings1 = text_lines[i-1].split()
+	strings2 = text_lines[j-1].split()
+	if text_name[0] == 'a': # get information from a all__.txt file
+		scan_no1 = strings1[1]
+		scan_no2 = strings2[1]
+		t1 = strings1[2]
+		t2 = strings2[2]
+	elif text_name[0] == 's': # get information from a scan__.txt file
+		scan_no1 = text_name[4:6]
+		scan_no2 = text_name[4:6]
+		t1 = strings1[0]
+		t2 = strings2[0]
+	print scan_no1,t1,scan_no2,t2
+
+	fn1 = '/cita/h/home-2/xzxu/trails/data/ek036a_ef_no00{}.m5a'.format(scan_no1)
+	fn2 = '/cita/h/home-2/xzxu/trails/data/ek036a_ef_no00{}.m5a'.format(scan_no2)
+	t_gp1 = Time(t1)
+	t_gp2 = Time(t2)
+	
 	gp1 = GP_data(fn1,t_gp1)
 	gp2 = GP_data(fn2,t_gp2)
-	gp2.plot_figs()    
-  	c = get_correlation_coefficients(gp1,gp2)
+	gp1.plot_figs() 
+	overlap_freq_spec(gp1,gp2)   
+  	
+  	c = get_correlation_coefficients(gp1,gp2) # This average one is better, as the one below takes into account non-data information, so returns a higher correlation coefficient
+  	#c1 = get_correlation_coefficient(gp1,gp2)
   	dt = t_gp2-t_gp1
   	dts = dt.sec 
   	print 'The correlation coefficient is {}, the time lag between the two pulses is {} seconds.'.format(c[0],dts)
--- EVN_crab/coeff_timelag.py
+++ EVN_crab/coeff_timelag.py
@@ -1,4 +1,4 @@
-from GPbaseband import *
+
 from baseband import mark5b
 import numpy as np
 from scipy.stats.stats import pearsonr
@@ -6,49 +6,84 @@ import astropy.units as u
 import matplotlib.pyplot as plt
 from astropy.time import Time
 
+from pickle import dump, load
+from os.path import exists 
+from GPbaseband import *
 
-text_name = 'all20.txt'
+text_name = 'all10.txt'
 with open(text_name, 'r') as f:
 	text = f.read()
 	text_lines = text.split('\n')
 
 print len(text_lines)
-tvalues = []
-cvalues = []
 
-for delta in [1]:	
-	for i in range(len(text_lines)-delta):
-		j = i+delta	
 
-		strings1 = text_lines[i].split()
-		strings2 = text_lines[j].split()
-		if text_name[0] == 'a': # get information from a all__.txt file
-			scan_no1 = strings1[1]
-			scan_no2 = strings2[1]
-			t1 = strings1[2]
-			t2 = strings2[2]
-		elif text_name[0] == 's': # get information from a scan__.txt file
-			scan_no1 = text_name[4:6]
-			scan_no2 = text_name[4:6]
-			t1 = strings1[0]
-			t2 = strings2[0]
+if exists('./figures/correlation_coeff/tvalues.txt'):
+	f=open('./figures/correlation_coeff/tvalues.txt','r') 
+	tvalues=load(f)
+	print len(tvalues)
+	f.close()
+else:
+	tvalues = []
+if exists('./figures/correlation_coeff/cvalues.txt'):
+	f=open('./figures/correlation_coeff/cvalues.txt','r') 
+	cvalues=load(f)
+	print len(cvalues)
+	f.close()
+else: 
+	cvalues= []
+
+def calculate_values(order):
+	with open('./figures/correlation_coeff/datalog{}.txt'.format(order), 'w') as f1:
+		f1.seek(0,2) #2 means end of file, 0 is the off_set
+		for delta in [3,4]:	
+			for i in range(0,len(text_lines)-delta-1):
+			#for i in [25]:
+				j = i+delta	
+
+				strings1 = text_lines[i].split()
+				strings2 = text_lines[j].split()
+				if text_name[0] == 'a': # get information from a all__.txt file
+					scan_no1 = strings1[1]
+					scan_no2 = strings2[1]
+					t1 = strings1[2]
+					t2 = strings2[2]
+				elif text_name[0] == 's': # get information from a scan__.txt file
+					scan_no1 = text_name[4:6]
+					scan_no2 = text_name[4:6]
+					t1 = strings1[0]
+					t2 = strings2[0]
 			
-		fn1 = '/cita/h/home-2/xzxu/trails/data/ek036a_ef_no00{}.m5a'.format(scan_no1)
-		fn2 = '/cita/h/home-2/xzxu/trails/data/ek036a_ef_no00{}.m5a'.format(scan_no2)
-		t_gp1 = Time(t1)
-		t_gp2 = Time(t2)
-		dt = t_gp2-t_gp1
-		if abs(dt.sec)<600 and abs(dt.sec)>10e-5:
-			try:	
-				gp1 = GP_data(fn1,t_gp1)
-				gp2 = GP_data(fn2,t_gp2)
-				c = get_correlation_coefficients(gp1,gp2)
-				tvalues.append(abs(dt.sec))
-				cvalues.append(c[0])
-				print scan_no1,t1,scan_no2,t2
-				print 'The correlation coefficient is {}, the time lag between the two pulses is {} seconds.'.format(c[0],dt.sec)
-			except AssertionError:
-				continue
+				fn1 = '/cita/h/home-2/xzxu/trails/data/ek036a_ef_no00{}.m5a'.format(scan_no1)
+				fn2 = '/cita/h/home-2/xzxu/trails/data/ek036a_ef_no00{}.m5a'.format(scan_no2)
+				t_gp1 = Time(t1)
+				t_gp2 = Time(t2)
+				dt = t_gp2-t_gp1
+				if abs(dt.sec)<100 and abs(dt.sec)>5e-6:
+					try:	
+						gp1 = GP_data(fn1,t_gp1)
+						gp2 = GP_data(fn2,t_gp2)
+						c = get_correlation_coefficients(gp1,gp2)
+						tvalues.append(abs(dt.sec))
+						cvalues.append(c[0])
+						pulse_times ='scan{}:{},scan{}:{}'.format(scan_no1,t1,scan_no2,t2)
+						statement = 'The correlation coefficient is {}, the time lag between the two pulses is {} seconds.'.format(c[0],dt.sec)
+						print pulse_times
+						print statement
+						f1.write(pulse_times+'\n'+statement+'\n') 
+				
+					except AssertionError:
+						print 'Assertion error:',scan_no1,t1,scan_no2,t2
+						continue
+				
+	f=open('./figures/correlation_coeff/tvalues.txt','w')
+	dump(tvalues,f)
+	f.close()
+	f=open('./figures/correlation_coeff/cvalues.txt','w')
+	dump(cvalues,f)
+	f.close()
+
+#calculate_values(2)
 
 plt.figure()
 plt.semilogx(tvalues,cvalues,'bo')
